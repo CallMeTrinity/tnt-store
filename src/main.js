44ddf7780eefd729b73aca105d1f18b011f9629f
antonin.p <antonin.p@lahautesociete.com>
2026-06-22 15:22:20 +0200

    feat: integrate PWA service worker with immediate update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+
+// autoUpdate: reload the page as soon as a freshly deployed SW takes control
+registerSW({ immediate: true })
 
 createApp(App).use(router).mount('#app')
